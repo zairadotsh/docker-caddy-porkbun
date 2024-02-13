2b1725f363d17f05824888464a188d3409bc3d26
viv32 <vivs7855@gmail.com>
2024-02-12 20:22:48 -0500

    fix version names and create draft release with actions

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-LATEST_MAJOR_VERSION=$(
+LATEST_PATCH_VERSION=$(
     cat Dockerfile | \
     head -n 1 | \
     grep -E -oh '[0-9](\.[0-9](\.[0-9])?)?$'
 )
 
 LATEST_MINOR_VERSION=$(
-    echo $LATEST_MAJOR_VERSION | \
-    grep -E -oh '[0-9](\.[0-9])'
+    echo $LATEST_PATCH_VERSION | \
+    grep -E -oh '^[0-9](\.[0-9])'
 )
 
-echo "Latest minor version: $LATEST_MAJOR_VERSION"
+echo "Latest minor version: $LATEST_PATCH_VERSION"
 echo "Latest major version: $LATEST_MINOR_VERSION"
 
 if [[ ! -z $GITHUB_OUTPUT ]]; then
-    echo "LATEST_MAJOR_VERSION=$LATEST_MAJOR_VERSION" >> $GITHUB_OUTPUT
-    echo "LATEST_MINOR_VERSION=$LATEST_MINOR_VERSION" >> $GITHUB_OUTPUT
+    echo "LATEST_MINOR_VERSION=$LATEST_MINOR_VERSION" >> $GITHUB_OUTPUT;
+    echo "LATEST_PATCH_VERSION=$LATEST_PATCH_VERSION" >> $GITHUB_OUTPUT;
 fi
