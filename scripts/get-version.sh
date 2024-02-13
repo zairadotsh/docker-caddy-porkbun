f85278cd7b42e4f580a7bcfdc9ccaa417e25f670
viv32 <vivs7855@gmail.com>
2024-02-12 19:35:25 -0500

    Update Actions to tag major and minor versions. Remove ghcr

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-LATEST_VERSION=$(
+LATEST_MAJOR_VERSION=$(
     cat Dockerfile | \
     head -n 1 | \
     grep -E -oh '[0-9](\.[0-9](\.[0-9])?)?$'
 )
 
-echo "Latest version: $LATEST_VERSION"
+LATEST_MINOR_VERSION=$(
+    echo $LATEST_MAJOR_VERSION | \
+    grep -E -oh '[0-9](\.[0-9])'
+)
+
+echo "Latest minor version: $LATEST_MAJOR_VERSION"
+echo "Latest major version: $LATEST_MINOR_VERSION"
 
 if [[ ! -z $GITHUB_OUTPUT ]]; then
-    echo "LATEST_VERSION=$LATEST_VERSION" >> $GITHUB_OUTPUT
+    echo "LATEST_MAJOR_VERSION=$LATEST_MAJOR_VERSION" >> $GITHUB_OUTPUT
+    echo "LATEST_MINOR_VERSION=$LATEST_MINOR_VERSION" >> $GITHUB_OUTPUT
 fi
